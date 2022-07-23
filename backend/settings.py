699ccbd54e347076a3e82afbfea20e56cd868db6
VanTien <tienle676@gmail.com>
2022-07-23 17:05:07 +0700

    [CONFIG] - add apps config

--- backend/settings.py
+++ backend/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'auth.apps.AuthConfig',
+    'user.apps.UserConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
