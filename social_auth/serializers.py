a7f6f4e5742f59a8525c73048f267343095e7689
tienlv14 <tienlv14@fpt.com.vn>
2023-05-24 19:51:16 +0700

    facebook login

--- social_auth/serializers.py
+++ social_auth/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
-from . import google
-from .services import register_social_google_user
+from . import google, facebook
+from .services import register_social_google_user, register_social_facebook_user
 from rest_framework.exceptions import AuthenticationFailed
 from django.conf import settings
+
 
 class GoogleSocialAuthSerializer(serializers.Serializer):
     auth_token = serializers.CharField()
@@ -23,3 +24,24 @@ class GoogleSocialAuthSerializer(serializers.Serializer):
         first_name = user_data.get('given_name', "")
         last_name = user_data.get("family_name", "")
         return register_social_google_user(email=email, first_name=first_name, last_name=last_name)
+
+
+class FacebookSocialAuthSerializer(serializers.Serializer):
+    """Handles serialization of facebook related data"""
+    auth_token = serializers.CharField()
+
+    def validate_auth_token(self, auth_token):
+        user_data = facebook.Facebook.validate(auth_token)
+        try:
+            if bool(user_data):
+                email = user_data.get('email')
+                name = user_data.get('name')
+                facebook_id = user_data.get('id')
+                return register_social_facebook_user(email=email, first_name=name, facebook_id=facebook_id)
+
+        except Exception as identifier:
+            pass
+
+        raise serializers.ValidationError(
+            {'error':'The token is invalid or expired. Please login again.'}
+        )
--- social_auth/views.py
+++ social_auth/views.py
@@ -1,7 +1,8 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
-from .serializers import GoogleSocialAuthSerializer
+from .serializers import GoogleSocialAuthSerializer, FacebookSocialAuthSerializer
+
 
 class GoogleSocialAuthView(GenericAPIView):
 
@@ -12,6 +13,22 @@ class GoogleSocialAuthView(GenericAPIView):
         POST with "auth_token"
         Send an idtoken as from google to get user information
         """
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = ((serializer.validated_data)['auth_token'])
+        return Response(data, status=status.HTTP_200_OK)
+
+
+class FacebookSocialAuthView(GenericAPIView):
+
+    serializer_class = FacebookSocialAuthSerializer
+
+    def post(self, request):
+        """
+        POST with "auth_token"
+        Send an access token as from facebook to get user information
+        """
+
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         data = ((serializer.validated_data)['auth_token'])
