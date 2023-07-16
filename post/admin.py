bc9d750b384465baa2dbfb2e08ccbc72d544e01e
VyTT <40004136+tranVyTDT@users.noreply.github.com>
2023-07-16 13:20:13 +0700

    [TTVy][fix migrations] fix migrations for post app

--- post/admin.py
+++ post/admin.py
@@ -1,20 +1,7 @@
 from django.contrib import admin
 
-from .models import Post, PostReview
+from .models import Post, PostDetail, PostReview
 
 
-# array = [Post, PostReview, Subject, RangeTime]
-# admin.site.register(array)
-
-# @admin.register(Post)
-# class PostDisplay(admin.ModelAdmin):
-#     list_display = (
-#         'id', 'author', 'teacher')
-#     search_fields = ('id', 'author', 'teacher')
-
-
-@admin.register(PostReview)
-class PostReviewDisplay(admin.ModelAdmin):
-    list_display = (
-        'id', 'post', 'comment', 'score', 't_create', 'is_edited',)
-    search_fields = ('id', 'post', 'comment', 'score', 't_create', 'is_edited',)
+array = [Post, PostReview, PostDetail]
+admin.site.register(array)
