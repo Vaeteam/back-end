3094b9c14a7e968c175e00897e11a1881543cca1
VyTT <40004136+tranVyTDT@users.noreply.github.com>
2023-07-23 21:44:58 +0700

    [VyTT][get subject api] add get subject api

--- common/serialiers.py
+++ common/serialiers.py
@@ -8,6 +8,21 @@ class SubjectSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+    def to_representation(self, instance):
+        children = Subject.objects.filter(parent_subject=instance.id)
+        subject = {
+            "key": instance.id,
+            "label": instance.name,
+            "data": instance.name,
+        }
+        children and subject.update({
+            "children": [
+               self.to_representation(sub) for sub in children
+            ]
+        })
+        return subject
+
+
 class RangeTimeSerializer(serializers.ModelSerializer):
     class Meta:
         model = RangeTime
--- common/urls.py
+++ common/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import get_subjects, get_range_time
 
 urlpatterns = [
-    path('get_subjects/', get_subjects, name="get_subjects"),
-    path('get_range_times/', get_range_time, name="get_range_time"),
+    path('subjects/', get_subjects, name="get_subjects"),
+    path('range_times/', get_range_time, name="get_range_time"),
 ]
