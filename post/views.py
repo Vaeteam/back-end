e3d13427ef7f7e4fa2ec43006bee2a148ad33486
VyTT <40004136+tranVyTDT@users.noreply.github.com>
2023-08-11 22:11:58 +0700

    [VyTT][CreatePost] enhence log

--- post/views.py
+++ post/views.py
@@ -1,3 +1,4 @@
+import logging
 from any_case import converts_keys
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -5,14 +6,22 @@ from .serializers import PostSerializer, PostDetailSerializer
 from rest_framework import status
 
 
+logger = logging.getLogger(__name__)
+
 @api_view(['POST'])
 def create(request):
-    """Create new Post."""
-    snake_case_request_data = converts_keys(request.data, case='snake')
-    post_serializer = PostDetailSerializer(data=snake_case_request_data)
+    """ Create new Post """
+    try:
+        logger.info(f"Create Post with data {str(request.data)}")
+        snake_case_request_data = converts_keys(request.data, case='snake')
+        post_serializer = PostDetailSerializer(data=snake_case_request_data)
 
-    if post_serializer.is_valid(raise_exception=True):
-        post_instance = post_serializer.save()
-        response_data = PostSerializer(post_instance).data
-        return Response(data=response_data, status=status.HTTP_201_CREATED)
-    return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
+        if post_serializer.is_valid(raise_exception=True):
+            post_instance = post_serializer.save()
+            response_data = PostSerializer(post_instance).data
+            logger.info(f"Created Post with data {str(response_data)}")
+            return Response(data=response_data, status=status.HTTP_201_CREATED)
+        
+    except Exception as e:
+        logger.info(f"Create Post failed with error {str(e)}")
+        return Response(data=str(e), status=status.HTTP_501_NOT_IMPLEMENTED)
