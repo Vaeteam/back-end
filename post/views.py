d6c61f62664b325e091fdfd3cdf5c5844ead31bd
tranVyTDT <40004136+tranVyTDT@users.noreply.github.com>
2023-08-16 22:54:41 +0700

    [VyTT][CreatePost] update response format

--- post/views.py
+++ post/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from .serializers import PostSerializer, PostDetailSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
-
+from common.utils import form_response
 
 logger = logging.getLogger(__name__)
 
@@ -15,7 +15,7 @@ def create(request):
     """ Create new Post """
     try:
         user_info = request.user
-        logger.info(f"Create Post with data {str(request.data)}")
+        logger.info(f"Creating Post with data {str(request.data)}")
         snake_case_request_data = converts_keys(request.data, case='snake')
         post_serializer = PostDetailSerializer(data=snake_case_request_data)
 
@@ -23,8 +23,16 @@ def create(request):
             post_instance = post_serializer.save()
             response_data = PostSerializer(post_instance).data
             logger.info(f"Created Post with data {str(response_data)}")
-            return Response(data=response_data, status=status.HTTP_201_CREATED)
-        
+            return Response(data=form_response(
+                                data=response_data, 
+                                message="Bài Tuyển Gia Sư", 
+                                detail="bài tuyển gia sư của bạn đã được tạo thành công, hãy đợi gia sư đến ứng tuyển"
+                                ), 
+                            status=status.HTTP_201_CREATED)
     except Exception as e:
         logger.error(f"Create Post failed with error {str(e)}")
-        return Response(data=str(e), status=status.HTTP_501_NOT_IMPLEMENTED)
+        return Response(data=form_response(
+            message=e.__class__.__name__, 
+            detail=str(e)
+            ), 
+            status=status.HTTP_501_NOT_IMPLEMENTED)
