3d973c4737dcb034cf66210ccd96f57a13587b44
tranVyTDT <40004136+tranVyTDT@users.noreply.github.com>
2023-08-23 23:02:56 +0700

    [VyTT][AdministrativeUnits] create model and api

--- common/views.py
+++ common/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Subject, RangeTime
-from .serialiers import SubjectSerializer, RangeTimeSerializer
+from .serialiers import AdministrativeUnitsSerializer, SubjectSerializer, RangeTimeSerializer
 
 
 @api_view(['GET'])
@@ -18,3 +18,11 @@ def get_range_time(request):
     range_times = RangeTime.objects.all()
     range_time_serializer = RangeTimeSerializer(range_times, many=True)
     return Response(status=status.HTTP_200_OK, data=range_time_serializer.data) 
+
+
+@api_view(['GET'])
+def get_administrative_unit(request, pk):
+    """Get all range times."""
+    administrative_units = RangeTime.objects.filter(pk=pk)
+    administrative_units_serializer = AdministrativeUnitsSerializer(administrative_units, many=True)
+    return Response(status=status.HTTP_200_OK, data=administrative_units_serializer.data) 
